f0e0a60e0662efb808248371b604b211dcc4f6fd
John Blackburn <jackblackburnn@icloud.com>
2025-03-25 18:18:42 -0400

    Move event handler creation to init

--- src/Game.cpp
+++ src/Game.cpp
@@ -22,7 +22,6 @@ Game::Game() : window(nullptr),
 	       battleMusic(nullptr)
 {
 
-	eventHandler = new EventHandler(this);
 
 	playButtonRect = { 270, 10, 60, 30 };
 	nextButtonRect = { 250, 10, 100, 30 };
@@ -36,30 +35,27 @@ Game::Game() : window(nullptr),
 
 
 bool Game::init() {
-
 		// Initialize SDL video and audio
         if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
         	std::cerr << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
 		return false;
         }
-
-
 		// Create Window
-	
 	window = SDL_CreateWindow("Battle Sim", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_SHOWN);
-
 	if (!window) {
 		std::cerr << "Window Creation Failed." << SDL_GetError() << std::endl;
 		return false;
 	}
-
+		// Create Renderer
 	SDL_Renderer* sdlRenderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (!sdlRenderer) {
 		std::cerr << "Renderer Creation Failed." << SDL_GetError() << std::endl;
 		return false;
 	}
-
 	renderer = new Renderer(sdlRenderer);
+
+		// Create Renderer
+	eventHandler = new EventHandler(this);
 
 		// initialize audio
 	int audio_rate = 44100;
